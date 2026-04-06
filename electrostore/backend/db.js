b24328135aa8b54cc2169246ca9929f38f2974ba
Soham51826 <sohamkirpekar@gmail.com>
2026-04-06 17:18:15 +0530

    backend

--- electrostore/backend/db.js
+++ electrostore/backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // update based on system
-    password: '', // update based on system
+    password: 'root', // update based on system
     database: 'electrostore',
     waitForConnections: true,
     connectionLimit: 10,
