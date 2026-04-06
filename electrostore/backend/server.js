ea1b34bd64c37ca1d75a04a080d26daf58b7bbf2
Soham51826 <sohamkirpekar@gmail.com>
2026-04-06 18:17:59 +0530

    full working website

--- electrostore/backend/server.js
+++ electrostore/backend/server.js
@@ -3,11 +3,24 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+const path = require('path');
+
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from the frontend directory
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 // Routes
 const usersRoutes = require('./routes/users');
+// ... (rest of routes)
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+app.use('/api/users', usersRoutes);
 const productsRoutes = require('./routes/products');
 const ordersRoutes = require('./routes/orders');
 const suppliersRoutes = require('./routes/suppliers');
